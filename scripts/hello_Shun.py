00e3a526a37648ae891fad30f4bbd443ad9d06dc
Shun-M-0728 <shun.moriguchi0728@berkeley.edu>
2025-09-22 12:03:06 -0700

    Add Shun's hello world script

---
+++ scripts/hello_Shun.py
@@ -0,0 +1,11 @@
+"""
+A simple hello world script by [Shun Moriguchi]
+"""
+name = "[Shun Moriguchi]"
+print(f"Hello from {name}!")
+print("This is my first script in our group project.")
+
+# Add a simple calculation
+favorite_number = 32  # Change this to your actual favorite number
+result = favorite_number * 2
+print(f"My favorite number times 2 is: {result}")
